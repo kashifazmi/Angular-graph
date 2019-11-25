02e89410b9b91f258423caa0e9eaf990aaa5167a
kashif <kashifazmi94@gmail.com>
2019-11-25 13:19:54 +0530

    Added high chart module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-graph';
+  Highcharts: typeof Highcharts = Highcharts; // required
+  chartConstructor = 'chart'; // optional string, defaults to 'chart'
+  chartOptions: Highcharts.Options = {
+    chart: {
+      type: 'column'
+  },
+  title: {
+      text: 'Change in GPTA Progress Per Calf'
+  },
+  xAxis: {
+      categories: ['Location Based', 'CLARIFIED Plus Beef/Sexed', 'CLARIFIED Plus Intermediate'],
+      title: {
+        text: null
+     }
+  },
+  yAxis: {
+      min: 190,
+      max: 250,
+      title: {
+          text: 'Genetic progress($)'
+      },
+      stackLabels: {
+          enabled: true,
+          style: {
+              fontWeight: 'bold',
+              color: ( // theme
+                  Highcharts.defaultOptions.title.style &&
+                  Highcharts.defaultOptions.title.style.color
+              ) || 'gray'
+          }
+      }
+  },
+  legend: {
+      align: 'right',
+      x: -30,
+      verticalAlign: 'top',
+      y: 25,
+      floating: true,
+      backgroundColor:
+          Highcharts.defaultOptions.legend.backgroundColor || 'white',
+      borderColor: '#CCC',
+      borderWidth: 1,
+      shadow: false
+  },
+  tooltip: {
+      headerFormat: '<b>{point.x}</b><br/>',
+      pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+  },
+  plotOptions: {
+      column: {
+          stacking: 'normal',
+          dataLabels: {
+              enabled: false
+          }
+      }
+  },
+  series: [
+    {
+     data: [210, 247, 233],
+     type: 'column',
+     name: 'GPTA Progress'
+    }
+  ]
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HighchartsChartModule } from 'highcharts-angular';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
